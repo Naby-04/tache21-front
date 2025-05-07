e6a955bd079a3b818b36070f6187b8a5d662b192
Naby-04 <smohamed.diop@gmail.com>
2025-05-07 12:57:22 +0100

    insertion modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import AdminDashboard from "./pages/Admin";
+import Modal from "./Composants/composants de la page admin/Modal";
 
 function App() {
   return (
     <>
       <AdminDashboard />
+      <Modal />
     </>
   );
 }
