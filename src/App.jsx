3f319d6da537552fd9a8291a7b4f42b0e9409620
Naby-04 <smohamed.diop@gmail.com>
2025-05-07 16:31:58 +0100

    reformage des div

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <AdminDashboard />
-      <Modal />
+      {/* <Modal /> */}
     </>
   );
 }
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,69 +1,57 @@
 import React from "react";
 import "../index.css";
+import SearchBar from "../Composants/composants de la page admin/SearchBar";
 
 const Admin = () => {
   return (
-    <div className="p-2 h-screen border rounded-md grid grid-cols-12 grid-rows-12 gap-1">
-      {/* partie composant navbar */}
-      <div className="p-2 col-span-10 col-start-3 row-span-2 border">
+    <div className="p-2 h-screen grid grid-cols-12 grid-rows-12 gap-1">
+      {/* Sidebar */}
+      <div className="p-2 col-span-3 row-span-12 row-start-1 border">
         <div
-          style={{
-            backgroundColor: "var(--primary-color)",
-          }}
-          className="p-3 rounded-md "
-        >
-          navbar
-        </div>
-      </div>
-      {/* partie composant navbar exit */}
-      {/* partie composant sidebar*/}
-      <div className="p-2 col-span-2 row-span-12 row-start-1 border">
-        <div
-          style={{
-            backgroundColor: "var(--primary-color)",
-          }}
+          style={{ backgroundColor: "var(--primary-color)" }}
           className="p-3"
         >
           sidebar
         </div>
       </div>
-      {/* partie composant sidebar exit*/}
-      {/* partie composant stats */}
-      <div className="p-2 col-span-7 row-span-5 border">
-        <div
-          style={{
-            backgroundColor: "var(--primary-color)",
-          }}
-          className="p-3"
-        >
-          stats
+
+      {/* Navbar */}
+      <div className="col-span-9 col-start-4 row-span-1 border flex items-center justify-center">
+        <SearchBar />
+      </div>
+
+      {/* Scrollable wrapper pour stats, table, diagramme */}
+      <div className="col-span-9 col-start-4 row-span-11 row-start-2 overflow-y-auto grid grid-cols-12 grid-rows-12 gap-1">
+        {/* Stats */}
+        <div className="p-2 col-span-9 row-span-8 border">
+          <div
+            style={{ backgroundColor: "var(--primary-color)" }}
+            className="p-3"
+          >
+            stats
+          </div>
+        </div>
+
+        {/* Table top rapports */}
+        <div className="p-2 row-span-5 col-span-12 border">
+          <div
+            style={{ backgroundColor: "var(--primary-color)" }}
+            className="p-3"
+          >
+            table top rapports
+          </div>
+        </div>
+
+        {/* Diagramme */}
+        <div className="p-2 col-span-3 row-span-8 col-start-10 row-start-1 border">
+          <div
+            style={{ backgroundColor: "var(--primary-color)" }}
+            className="p-3"
+          >
+            Diagramme
+          </div>
         </div>
       </div>
-      {/* partie composant stats exit*/}
-      {/* partie composant Table utilisateur*/}
-      <div className="p-2 sm:col-span-7 row-span-5 col-span-10 border">
-        <div
-          style={{
-            backgroundColor: "var(--primary-color)",
-          }}
-          className="p-3"
-        >
-          table utilisateur
-        </div>
-      </div>
-      {/* partie composant Table utilisateur exit*/}
-      {/* partie composant liste top rapports */}
-      <div className="p-2 col-span-3 sm:row-span-10 sm:row-start-3 row-span-5 sm:col-start-10 col-start-10 row-start-3 border">
-        <div
-          style={{
-            backgroundColor: "var(--primary-color)",
-          }}
-          className="p-3"
-        >
-          liste top rapports
-        </div>
-      </div>
-      {/* partie composant liste top rapports exit*/}
     </div>
   );
 };
