9b553b712499662ceb98c0dd45e5fdec97eb46f0
Naby-04 <smohamed.diop@gmail.com>
2025-05-11 00:11:22 +0100

    comment admin

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,24 @@ import { DashboardUsers } from "./pages/DashboardUsers/DashboardUsers";
 import HomePage from "./pages/HomePage";
 import Inscription from "./pages/Inscription";
 import Connexion from "./pages/Connexion";
-import Admin from "./pages/Admin";
+// import Admin from "./pages/Admin";
 
 const App = () => {
   return (
-    <Admin />
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<HomePage />} />
-    //     <Route path="/inscription" element={<Inscription />} />
-    //     <Route path="/connexion" element={<Connexion />} />
-    //     <Route path="/users" element={<DashboardUsers />}>
-    //       <Route index element={<Acceuil />} />
-    //       <Route path="rapport" element={<Rapport />} />
-    //       <Route path="rapportTelecharger" element={<RapportTelecharger />} />
-    //     </Route>
-    //     <Route path="/pageParametre" element={<PageParametresCompte />} />
-    //   </Routes>
-    // </BrowserRouter>
+    // <Admin />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/inscription" element={<Inscription />} />
+        <Route path="/connexion" element={<Connexion />} />
+        <Route path="/users" element={<DashboardUsers />}>
+          <Route index element={<Acceuil />} />
+          <Route path="rapport" element={<Rapport />} />
+          <Route path="rapportTelecharger" element={<RapportTelecharger />} />
+        </Route>
+        <Route path="/pageParametre" element={<PageParametresCompte />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
