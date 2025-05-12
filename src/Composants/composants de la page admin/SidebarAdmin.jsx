d9918f9894243035f062a266f03e9e4dcef700b3
Naby-04 <smohamed.diop@gmail.com>
2025-05-12 10:23:49 +0100

    pagination et modif sidebar

--- src/Composants/composants de la page admin/SidebarAdmin.jsx
+++ src/Composants/composants de la page admin/SidebarAdmin.jsx
@@ -1,45 +1,89 @@
+import { useState } from "react";
 import { RiDashboardHorizontalFill } from "react-icons/ri";
 import { HiUsers } from "react-icons/hi2";
 import { HiDocumentReport } from "react-icons/hi";
 import { CiLogout } from "react-icons/ci";
 
-const SidebarAdmin = ({setVueActive}) => {
-    return ( 
-      <aside className="w-20 md:w-64 bg-gray-800 text-white px-5 pt-1 pb-2 flex flex-col static overflow-y-auto transition-all duration-200">
+const SidebarAdmin = ({ setVueActive }) => {
+  const [activeItem, setActiveItem] = useState("dashboard"); // par défaut
 
-        <div className="flex p-2 rounded gap-2 items-center border-b border-amber-300">
-            <div className="w-8 h-8 relative rounded-full border-amber-300 border">
-                <img src="" alt="" className="absolute w-full h-full object-cover rounded-full" />
-            </div>
-            <h4 className="text-white text-xl font-bold uppercase hidden md:block">SenRapport</h4>
+  const handleItemClick = (vue) => {
+    setVueActive(vue);
+    setActiveItem(vue);
+  };
+
+  return (
+    <aside className="w-20 md:w-64 bg-gray-800 text-white px-5 pt-1 pb-2 flex flex-col static overflow-y-auto transition-all duration-200">
+      <div className="flex p-2 rounded gap-2 items-center border-b border-amber-300">
+        <div className="w-8 h-8 relative rounded-full border-amber-300 border">
+          <img
+            src=""
+            alt=""
+            className="absolute w-full h-full object-cover rounded-full"
+          />
         </div>
+        <h4 className="text-white text-xl font-bold uppercase hidden md:block">
+          SenRapport
+        </h4>
+      </div>
 
-        <div className="mt-10">
-            <h5 className="text-amber-100 hidden md:block">Dashboard</h5>
+      <div className="mt-10">
+        <h5 className="text-amber-100 hidden md:block">Dashboard</h5>
 
-            <ul className="space-y-2">
-            <li onClick={() => setVueActive("dashboard")} className="cursor-pointer flex items-center justify-center md:justify-start py-3 mt-2 gap-2 border-b border-amber-200">
-                <span className="p-2 bg-amber-200 rounded-full text-gray-800">{<RiDashboardHorizontalFill />}</span>
-                <span className="hidden md:inline">Dashboards</span>
-            </li>
-            <li onClick={() => setVueActive("users")} className="cursor-pointer flex items-center justify-center md:justify-start py-3 mt-2 gap-2 border-b border-amber-200">
-                <span className="p-2 bg-amber-200 rounded-full text-gray-800">{<HiUsers />}</span>
-                <span className="hidden md:inline">Utilisateurs</span>
-            </li>
-            <li onClick={() => setVueActive("rapports")} className="cursor-pointer flex items-center justify-center md:justify-start py-3 mt-2 gap-2 border-b border-amber-200">
-                <span className="p-2 bg-amber-200 rounded-full text-gray-800">{<HiDocumentReport />}</span>
-                <span className="hidden md:inline">Rapports</span>
-            </li>
-            </ul>
-        </div>
+        <ul className="space-y-2">
+          <li
+            onClick={() => handleItemClick("dashboard")}
+            className={`cursor-pointer flex items-center justify-center md:justify-start py-3 px-2 mt-2 gap-2 border-b border-amber-200 rounded-md transition-colors duration-150
+              ${
+                activeItem === "dashboard"
+                  ? "bg-amber-100/20"
+                  : "hover:bg-amber-100/20"
+              }`}
+          >
+            <span className="p-2 bg-amber-200 rounded-full text-gray-800">
+              {<RiDashboardHorizontalFill />}
+            </span>
+            <span className="hidden md:inline">Dashboards</span>
+          </li>
 
-        <div className="flex items-center justify-center md:justify-start cursor-pointer gap-2 mt-auto pt-6 text-4">
-            {<CiLogout />}
-            <p className="text-amber-50 hidden md:inline">Déconnexion</p>
-        </div>
-        </aside>
+          <li
+            onClick={() => handleItemClick("users")}
+            className={`cursor-pointer flex items-center justify-center md:justify-start py-3 px-2 mt-2 gap-2 border-b border-amber-200 rounded-md transition-colors duration-150
+              ${
+                activeItem === "users"
+                  ? "bg-amber-100/20"
+                  : "hover:bg-amber-100/20"
+              }`}
+          >
+            <span className="p-2 bg-amber-200 rounded-full text-gray-800">
+              {<HiUsers />}
+            </span>
+            <span className="hidden md:inline">Utilisateurs</span>
+          </li>
 
-     );
-}
- 
+          <li
+            onClick={() => handleItemClick("rapports")}
+            className={`cursor-pointer flex items-center justify-center md:justify-start py-3 px-2 mt-2 gap-2 border-b border-amber-200 rounded-md transition-colors duration-150
+              ${
+                activeItem === "rapports"
+                  ? "bg-amber-100/20"
+                  : "hover:bg-amber-100/20"
+              }`}
+          >
+            <span className="p-2 bg-amber-200 rounded-full text-gray-800">
+              {<HiDocumentReport />}
+            </span>
+            <span className="hidden md:inline">Rapports</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex items-center justify-center md:justify-start cursor-pointer gap-2 mt-auto pt-6 text-4">
+        <CiLogout />
+        <p className="text-amber-50 hidden md:inline">Déconnexion</p>
+      </div>
+    </aside>
+  );
+};
+
 export default SidebarAdmin;
--- src/Composants/composants de la page admin/TableUser.jsx
+++ src/Composants/composants de la page admin/TableUser.jsx
@@ -1,67 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdMode } from "react-icons/md";
 import { FaTrash } from "react-icons/fa";
 
+const TableUser = ({ tabUsers }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const usersPerPage = 10;
 
-const TableUser = ({tabUsers}) => {
+  // Calcule les indexs de début et de fin
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = tabUsers.slice(indexOfFirstUser, indexOfLastUser);
+
+  const totalPages = Math.ceil(tabUsers.length / usersPerPage);
+
+  const nextPage = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
   return (
     <div className="bg-white rounded-lg shadow-md p-4">
-          <h2 className="text-xl font-semibold mb-4">Listes des Utilisateurs</h2>
+      <h2 className="text-xl font-semibold mb-4">Listes des Utilisateurs</h2>
 
-          <div className="overflow-x-auto w-full">
-            <table className="w-full text-left min-w-[600px]">
-              <thead>
-                <tr className="bg-gray-800 text-gray-50 rounded">
-                  <th className="py-2 px-3">#</th>
-                  <th className="py-2 px-3">Nom</th>
-                  <th className="py-2 px-3">Role</th>
-                  <th className="py-2 px-3">Date</th>
-                  <th className="py-2 px-3">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {tabUsers.map((tab, index) => (
-                  <tr key={index} className="border-t">
-                    <td className="py-2 px-3 font-bold text-gray-600">
-                      {tab.id}
-                    </td>
-                    <td className="py-2 px-3 flex items-center">
-                      <img
-                        src={tab.image}
-                        alt="profil"
-                        className="w-12 h-12 object-cover rounded"
-                      />
-                      <h4 className="font-semibold">{tab.name}</h4>
-                    </td>
-                    <td className="py-2 px-3">
-                      <h4 className="font-semibold">{tab.role}</h4>
-                    </td>
-                    <td className="py-2 px-3">
-                      <h4 className="font-semibold">{tab.jourInscripte}</h4>
-                    </td>
-                    <td className="px-3 py-2">
-                      <div className="flex items-center gap-3">
-                        <button
-                          onClick={() => tab.onDetailClick()}
-                          className="p-2 rounded bg-amber-200 text-amber-800 hover:bg-amber-400"
-                        >
-                          <MdMode />
-                        </button>
-                        <button
-                          onClick={() => tab.onDeleteClick()}
-                          className="p-2 rounded bg-red-100 text-red-700 hover:bg-red-200"
-                        >
-                          <FaTrash />
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+      <div className="overflow-x-auto w-full">
+        <table className="w-full text-left min-w-[600px]">
+          <thead>
+            <tr className="bg-gray-800 text-gray-50 rounded">
+              <th className="py-2 px-3">#</th>
+              <th className="py-2 px-3">Nom</th>
+              <th className="py-2 px-3">Role</th>
+              <th className="py-2 px-3">Date</th>
+              <th className="py-2 px-3">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentUsers.map((tab, index) => (
+              <tr key={index} className="border-t">
+                <td className="py-2 px-3 font-bold text-gray-600">{tab.id}</td>
+                <td className="py-2 px-3 flex items-center gap-2">
+                  <img
+                    src={tab.image}
+                    alt="profil"
+                    className="w-12 h-12 object-cover rounded"
+                  />
+                  <h4 className="font-semibold">{tab.name}</h4>
+                </td>
+                <td className="py-2 px-3">
+                  <h4 className="font-semibold">{tab.role}</h4>
+                </td>
+                <td className="py-2 px-3">
+                  <h4 className="font-semibold">{tab.jourInscripte}</h4>
+                </td>
+                <td className="px-3 py-2">
+                  <div className="flex items-center gap-3">
+                    <button
+                      onClick={() => tab.onDetailClick()}
+                      className="p-2 rounded bg-amber-200 text-amber-800 hover:bg-amber-400"
+                    >
+                      <MdMode />
+                    </button>
+                    <button
+                      onClick={() => tab.onDeleteClick()}
+                      className="p-2 rounded bg-red-100 text-red-700 hover:bg-red-200"
+                    >
+                      <FaTrash />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {/* PAGINATION */}
+        <div className="flex justify-center gap-7 items-center mt-4">
+          <button
+            onClick={prevPage}
+            disabled={currentPage === 1}
+            className="px-4 py-2 bg-gray-800 rounded hover:bg-gray-300 disabled:opacity-50 text-white"
+          >
+            Précédent
+          </button>
+          <span className="text-gray-600">
+            Page {currentPage} sur {totalPages}
+          </span>
+          <button
+            onClick={nextPage}
+            disabled={currentPage === totalPages}
+            className="px-4 py-2 bg-gray-800 rounded hover:bg-gray-300 disabled:opacity-50 text-white"
+          >
+            Suivant
+          </button>
         </div>
-  )
+      </div>
+    </div>
+  );
 };
 
 export default TableUser;
