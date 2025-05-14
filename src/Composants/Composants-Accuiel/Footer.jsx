1edcd3c7b79e5ba590d9b529f138e1c4841cae75
Nafi-Badji <badjimoretalla@gmail.com>
2025-05-14 18:03:43 +0000

    hgiujuitg

--- src/Composants/Composants-Accuiel/Footer.jsx
+++ src/Composants/Composants-Accuiel/Footer.jsx
@@ -10,6 +10,8 @@ import { MdOutlineEmail } from "react-icons/md";
 import { db, serverTimestamp } from "../Composants-Accuiel/firebase";
 import { initializeApp } from "firebase/app";
 import { collection, addDoc } from "firebase/firestore";
+import { Link } from "react-router-dom";
+
 
 
 const Footer = () => {
@@ -57,32 +59,32 @@ const Footer = () => {
           </div>
           {/* ==============partie rapports============== */}
           <div className="flex flex-col gap-4  text-white">
-            <p className="text-xl font-medium text-amber-300">Rapports</p>
+            {/* <p className="text-xl font-medium text-amber-300">Rapports</p> */}
             <div className="flex flex-col gap-4">
-              <div className="hover:text-gray-300">
-                <a href="#">Médecine et Santé</a>
+              <div className="hover:text-gray-300 text-amber-300">
+                <Link to='/'>Accueil</Link>
               </div>
               <div className="hover:text-gray-300">
-                <a href="#">Droit Privé</a>
+                <Link to='/'>Service</Link>
               </div>
               <div className="hover:text-gray-300">
-                <a href="#">Sociologie</a>
+                <Link to='/'>Rapports</Link>
               </div>
               <div className="hover:text-gray-300">
-                <a href="#">Sciences</a>
+                <Link to='/'>À propos</Link>
               </div>
             </div>
           </div>
           {/*================partie contact================ */}
           <div className="flex flex-col gap-4  text-white">
-          <p className="text-xl  font-medium text-amber-300">Contacts</p>
+          <p className="text-sm  text-amber-300">Contacts</p>
             <div>
               <div className="flex gap-1 hover:text-gray-300">
                  <div className="text-xl text-amber-300">
                  <IoLocation />
                  </div>
                  <div>
-                 <a href="#">Dakar, Sénégal</a>
+                 <Link to='/'>Dakar, Sénégal</Link>
                  </div>
               </div>
             </div>
@@ -92,7 +94,7 @@ const Footer = () => {
                 <FaPhoneAlt />
                 </div>
                 <div>
-                <a href="#">76 592 61 27</a>
+                <Link to='/'>76 592 61 27</Link>
                 </div>
               </div>
             </div>
@@ -102,7 +104,7 @@ const Footer = () => {
                 <MdOutlineEmail />
                 </div>
                 <div>
-                <a href="#">hello@senrapports.com</a>
+                <Link to='/'>hello@senrapports.com</Link>
                 </div>
               </div>
             </div>
@@ -112,7 +114,7 @@ const Footer = () => {
             <div>
               <label
                 htmlFor="newsletter"
-                className="block text-sm/6 font-medium text-amber-300"
+                className="block text-sm text-amber-300"
               >
                 NEWSLETTER:
               </label>
@@ -142,15 +144,15 @@ const Footer = () => {
             </div>
             <div className="flex flex-col gap-6 mt-8">
               <div className="flex gap-2 flex-wrap text-2xl">
-                <a href="#" className=" text-gray-300 hover:text-white">
+                <Link  to='/' className=" text-gray-300 hover:text-white">
                   <FaFacebook />
-                </a>
-                <a href="#" className=" text-gray-300 hover:text-white">
+                </Link>
+                <Link to='/' className=" text-gray-300 hover:text-white">
                   <FaLinkedin />
-                </a>
-                <a href="#" className=" text-gray-300 hover:text-white">
+                </Link>
+                <Link to='/' className=" text-gray-300 hover:text-white">
                   <FaXTwitter />
-                </a>
+                </Link>
               </div>
             </div>
             {/* ==================Button================== */}
