3415337d2f8f508ee70027d2be5d198e785ef5fa
Hamidou Ly <hamidesn221@gmail.com>
2025-05-15 16:14:31 +0100

    correctrion des page d'authenfication

--- src/pages/MotDePassOublie.jsx
+++ src/pages/MotDePassOublie.jsx
@@ -50,6 +50,7 @@ function MotDePassOublie() {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="email"
               type="email"
+              autoComplete="email"
               placeholder="Votre email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
