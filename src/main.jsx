8fbc21f2d196a5eae11aa0ed2447d779a41ebaad
Mega17Z <planimportant@gmail.com>
2025-05-15 13:28:36 +0000

    premiere page du pdf reussit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { pdfjs } from 'react-pdf';
+
+// 📌 Pour charger le worker depuis le CDN (évite les erreurs de chemin/mime)
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@4.8.69/build/pdf.worker.min.mjs`;
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
