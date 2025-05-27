75b6affa3350b9d36d0e7df197076d34a6a77bb4
Nafi-Badji <badjimoretalla@gmail.com>
2025-05-27 08:33:52 +0000

    pushing

--- src/Composants/Composants-Accuiel/Footer.jsx
+++ src/Composants/Composants-Accuiel/Footer.jsx
@@ -188,7 +188,7 @@ const Footer = () => {
               SenRapports © 2025. Tous droits réservés.
             </p>
           </div>
-          <div className="px-4">
+          {/* <div className="px-4">
             <button
               onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
               className="fixed bottom-5 right-5 bg-amber-300 text-white p-3 rounded-full shadow-lg  transition z-50 btnAnime"
@@ -198,7 +198,7 @@ const Footer = () => {
                 <FaLongArrowAltUp />
               </span>
             </button>
-          </div>
+          </div> */}
         </div>
       </footer>
     </>
