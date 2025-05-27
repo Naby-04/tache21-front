f960fed44269424d2630f1bfb78fec698b153446
binta dia <bintadia269@gmail.com>
2025-05-27 11:28:37 +0200

    chat

--- src/Composants/components/ChatbotWidget.jsx
+++ src/Composants/components/ChatbotWidget.jsx
@@ -12,7 +12,7 @@ const ChatbotWidget = () => {
     <div className="fixed bottom-6 right-6 z-50">
       {open && (
         <div
-          className="mb-2 w-[260px] h-[400px] rounded-lg shadow-lg border border-gray-200 overflow-hidden animate-fade-slide"
+          className="mb-2 w-[380px] h-[430px] rounded-lg shadow-lg border-4 border-gray-200 overflow-hidden animate-fade-slide"
         >
           <iframe
             src="https://typebot.co/faq-n5ywjzo"
